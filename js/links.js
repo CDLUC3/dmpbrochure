538e1e18dd7b23c9a0345cb7545afb59c43c133b
briley <brian.riley@ucop.edu>
2016-03-04 11:13:45 -0800

    Updated the links to point to prod

--- js/links.js
+++ js/links.js
@@ -1,6 +1,6 @@
 var rewrites = [
-  {marker: "home_page", target: "https://dmp2-dev.cdlib.org/"},
-  {marker: "community_resources", target: "https://dmp2-dev.cdlib.org/community_resources"},
-  {marker: "privacy_policy", target: "https://dmp2-dev.cdlib.org/privacy/"},
-  {marker: "terms_of_use", target: "https://dmp2-dev.cdlib.org/terms_of_use/"}
+  {marker: "home_page", target: "http://www.dmptool.org/"},
+  {marker: "community_resources", target: "http://www.dmptool.org/community_resources"},
+  {marker: "privacy_policy", target: "http://www.dmptool.org/privacy/"},
+  {marker: "terms_of_use", target: "http://www.dmptool.org/terms_of_use/"}
 ];
