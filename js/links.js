79134d6afe1f519acbe4e3863ba8d3b5c7fb2a48
dmp2 role account <dmp2@uc3-dmp2-stg.cdlib.org>
2016-03-08 11:44:22 -0800

    moved links.js to a shared directory

--- js/links.js
+++ js/links.js
@@ -1,6 +1,1 @@
-var rewrites = [
-  {marker: "home_page", target: "http://www.dmptool.org/"},
-  {marker: "community_resources", target: "http://www.dmptool.org/community_resources"},
-  {marker: "privacy_policy", target: "http://www.dmptool.org/privacy/"},
-  {marker: "terms_of_use", target: "http://www.dmptool.org/terms_of_use/"}
-];
+/dmp2/apps/dmp2brochure/shared/js/links.js
